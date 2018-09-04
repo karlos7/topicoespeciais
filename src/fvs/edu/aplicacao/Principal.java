abe7682314fc5bc4f2eab1f584a390b4a015066b
Karlos Oliveira <karlos.1914.so@gmail.com>
2018-09-04 20:05:21 -0300

    Aula JPA 2

--- src/fvs/edu/aplicacao/Principal.java
+++ src/fvs/edu/aplicacao/Principal.java
@@ -13,12 +13,15 @@ public class Principal {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("topicos-fvs");
 		EntityManager em = emf.createEntityManager();
 		
-		em.getTransaction().begin();
 		
-		Aluno aluno = new Aluno(null, "Karlos Oliveira", "035.919.683-74", "132421312", 9131);
-		em.persist(aluno);
-		em.getTransaction().commit();
-		em.close();
+		Aluno aluno = em.find(Aluno.class, 3);
+		System.out.println(aluno);
+		
+		//em.getTransaction().begin();
+		//Aluno aluno = new Aluno(null, "Karlos Oliveira", "035.919.683-74", "132421312", 9131);
+		//em.persist(aluno);
+		//em.getTransaction().commit();
+		
 		System.out.println("Salvo com sucesso!");
 		
 
