e5a1bb9d9928a77fd53906cf97eaab0d7e6274ac
Bonfim <bonfimadonias@gmail.com>
2018-10-23 19:56:57 -0300

    Implementação do relacionamento entre pedido e produto

---
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/domain/ItemPedido.java
@@ -0,0 +1,78 @@
+package fvs.edu.br.topicos.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class ItemPedido implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@JsonIgnore
+	@EmbeddedId
+	private ItemPedidoPK id = new ItemPedidoPK();
+	
+	private Double desconto;
+	private Double preco;
+	private Integer quantidade;
+
+	public ItemPedido() {}
+
+	public ItemPedido(Pedido pedido, Produto produto, ItemPedidoPK id, Double desconto, Double preco, Integer quantidade) {
+		super();
+		id.setPedido(pedido);
+		id.setProduto(produto);
+		this.id = id;
+		this.desconto = desconto;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
+	
+	public Produto getProduto() {
+		return id.getProduto();
+	}
+	
+	@JsonIgnore
+	public Pedido getPedido() {
+		return id.getPedido();
+	}
+	
+	public ItemPedidoPK getId() {
+		return id;
+	}
+
+	public void setId(ItemPedidoPK id) {
+		this.id = id;
+	}
+
+	public Double getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(Double desconto) {
+		this.desconto = desconto;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	
+	
+}
